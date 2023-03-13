0ac593ff90021820081b7fad0d30d7b03b5d50d1
subzero <shubham8111994@gmail.com>
2023-03-13 14:02:13 +0530

    Added feature to count entries made

--- src/main/java/org/employee/EmployeePayrollServices.java
+++ src/main/java/org/employee/EmployeePayrollServices.java
@@ -10,6 +10,7 @@ public class EmployeePayrollServices
     public  static List<EmployeePayrollData> empList = new ArrayList<EmployeePayrollData>();
     public static ObjectOutputStream oos =null;
     public static ObjectInputStream ois = null;
+    public static int entries = 0;
     public static File file = new File("Employee.txt");
     public void readEmployeeData(Scanner scanner) throws Exception{
         System.out.println("How many records you want to enter");
@@ -26,12 +27,20 @@ public class EmployeePayrollServices
         oos = new ObjectOutputStream(new FileOutputStream(file));
         oos.writeObject(empList);
         oos.close();
+        entries++;
 
 }
     public void writeEmployeeData(){
         System.out.println("--------------------------------");
         empList.forEach(System.out::println);
         System.out.println("--------------------------------");
+    }
+
+    public void countEntries(){
+            System.out.println("--------------------------------");
+            System.out.println("entries made: "+entries);
+            System.out.println("--------------------------------");
+
     }
     public static void main( String[] args ) throws Exception {
         Scanner scanner = new Scanner(System.in);
@@ -43,12 +52,14 @@ public class EmployeePayrollServices
             ois.close();
         }
         while (choice!=0){
-            System.out.println("Enter the choice to perform\n1.ADD \n2.DISPLAY \n0.EXIT");
+            System.out.println("Enter the choice to perform\n1.ADD \n2.DISPLAY \n3.COUNT ENTRIES \n0.EXIT");
             choice = scanner.nextInt();
             switch (choice){
                 case 1:emp1.readEmployeeData(new Scanner(System.in));
                 break;
                 case 2:emp1.writeEmployeeData();
+                    break;
+                case 3:emp1.countEntries();
                     break;
                 case 0:choice=0;
                     break;
